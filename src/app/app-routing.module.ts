1d8f50ec3b040132c31268d615074a1cf0fa0b1e
MinaOnsy24 <onsymina676@gamil.com>
2023-06-03 01:25:55 +0300

    done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //   {
+  //   path:"",
+  //   component:LoginComponent
+
+  // },
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'singup',
+    component:SignupComponent
+  }
+  // {
+  //   path:"**",
+  //   component:
+  // }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+// const routes: Routes = [
+// {
+//   path:"navbar",
+//   component:NavbarComponent
+// },
+// {
+//   path:"footer",
+//   component:FooterComponent
+// }
+
+// ];
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+  ],
+  exports: [
+    NavbarComponent,
+    FooterComponent
+  ]
+
+})
+export class SharedModule { }
